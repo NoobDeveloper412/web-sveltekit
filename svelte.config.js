5b2b4fb0737d8bcdde968756ec4067a240a36f40
NoobDeveloper412 <mzuhair412@gmail.com>
2024-08-28 16:15:37 +0500

    gh-pagers

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,31 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$components: 'src/lib/components',
-			$json: 'src/lib/json',
-			$layouts: 'src/lib/layouts',
-			$utils: 'src/lib/utils',
-			$pages: 'src/lib/pages',
-			$lib: 'src/lib',
-			$styles: 'src/lib/styles',
-			$routes: 'src/routes'
-		}
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null, // Use '200.html' if you prefer SPA behavior
+            strict: false   // Set to false to allow non-prerenderable routes
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/my-repo' : '',
+        },
+        alias: {
+            $components: 'src/lib/components',
+            $json: 'src/lib/json',
+            $layouts: 'src/lib/layouts',
+            $utils: 'src/lib/utils',
+            $pages: 'src/lib/pages',
+            $lib: 'src/lib',
+            $styles: 'src/lib/styles',
+            $routes: 'src/routes'
+        }
+    }
 };
 
 export default config;
